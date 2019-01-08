d3483bb9358b8b402644d48a9a76b801296f5d69
Aidan Prehatny <aprehatny@yahoo.com>
2019-01-08 00:36:00 -0800

    add launch reducer

--- server/src/datasources/launch.js
+++ server/src/datasources/launch.js
@@ -7,8 +7,26 @@ class LaunchAPI extends RESTDataSource {
   }
 
   async getAllLaunches() {
-    const res = await this.get('launches');
-    return res && res.length ? res.map(l => this.launchReducer(l)) : [];
+    const res = await this.get('launches'); // underlying spacex restful get helper method
+    return res && res.length ? res.map(l => this.launchReducer(l)) : []; // map launch data from restful response
+  }
+
+  launchReducer(launch) { // transform launch data into exptected schema shape
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
   }
 }
 
