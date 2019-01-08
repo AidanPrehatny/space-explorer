3afe469b1250edbb300cd02064ca7b756b3a4274
Aidan Prehatny <aprehatny@yahoo.com>
2019-01-07 23:53:06 -0800

    shape our query, add initial Launch obj type

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,4 +2,19 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql``
 
+type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    # Queries for the current user
+    me: User
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
 module.exports = typeDefs;
