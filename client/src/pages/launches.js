8ccc046fd5d3ee6196f082f44b80666edd750fae
Aidan Prehatny <aprehatny@yahoo.com>
2019-01-15 00:05:12 -0800

    add fetch more for pagination and load more of query

--- client/src/pages/launches.js
+++ client/src/pages/launches.js
@@ -30,25 +30,55 @@ const GET_LAUNCHES = gql`
 
 export default function Launches() {
     return (
-      <Query query={GET_LAUNCHES}>
-        {({ data, loading, error }) => {
-          if (loading) return <Loading />;
-          if (error) return <p>ERROR</p>;
-  
-          return (
-            <Fragment>
-              <Header />
-              {data.launches &&
-                data.launches.launches &&
-                data.launches.launches.map(launch => (
-                  <LaunchTile
-                    key={launch.id}
-                    launch={launch}
-                  />
-                ))}
-            </Fragment>
-          );
-        }}
-      </Query>
+        <Query query={GET_LAUNCHES}>
+            {({ data, loading, error, fetchMore }) => {
+                if (loading) return <Loading />;
+                if (error) return <p>ERROR</p>;
+
+                return (
+                    <Fragment>
+                        <Header />
+                        {data.launches &&
+                            data.launches.launches &&
+                            data.launches.launches.map(launch => (
+                                <LaunchTile
+                                    key={launch.id}
+                                    launch={launch}
+                                />
+                            ))}
+
+                        {data.launches &&
+                            data.launches.hasMore && (
+                                <Button
+                                    onClick={() =>
+                                        fetchMore({
+                                            variables: {
+                                                after: data.launches.cursor,
+                                            },
+                                            updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                                                if (!fetchMoreResult) return prev;
+                                                return {
+                                                    ...fetchMoreResult,
+                                                    launches: {
+                                                        ...fetchMoreResult.launches,
+                                                        launches: [
+                                                            ...prev.launches.launches,
+                                                            ...fetchMoreResult.launches.launches,
+                                                        ],
+                                                    },
+                                                };
+                                            },
+                                        })
+                                    }
+                                >
+                                    Load More
+                        </Button>
+                            )
+                        }
+
+                    </Fragment>
+                );
+            }}
+        </Query>
     );
-  };
+};
